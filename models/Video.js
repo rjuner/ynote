622a9cd847df2351e7a27357469dfd0ad27441e4
Rhea Rivera <rjuner@gmail.com>
2016-09-27 12:03:38 -0500

    database test

--- models/Video.js
+++ models/Video.js
@@ -3,28 +3,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var VideoSchema = new Schema({
-
-  //  This is input in a form by User
-  title: {
-    type:String,
-    unique:true, 
-    required: true
-  }, 
   url: {
     type:String,
     unique: true, 
     required: true
-
   }, 
   duration: {
-    type: Number, 
+    type: String, 
     required: true
   }, 
   comments: [{
       type: Schema.Types.ObjectId,
       ref: 'Comment'
-  }], 
-
+  }]
 
 });
 
--- server.js
+++ server.js
@@ -1,66 +1,96 @@
-//	DEPENDENCIES
+//	Dependencies
 var express = require('express'); 
 var app = express(); 
 var bodyParser = require('body-parser'); 
 var logger = require('morgan'); 
-var mongoose = require('mongoose'); 
+var mongoose = require('mongoose');
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
 
-//	use morgan and bodyparser 
-app.use(logger('dev')); 
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
 
-app.use(express.static('public')); 
+// Run Morgan for Logging
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-mongoose.connect('mongodb://localhost/ynoteDB'); 
-var db = mongoose.connection; 
+app.use(express.static('./public'));
 
-//	show errors 
-db.on('error', function(err){
-	console.log('Mongoose Error: ', err); 
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Setting up mongoDB 
+mongoose.connect('mongodb://localhost/vonntest');
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+	console.log('Mongoose Error: ', err);
 });
 
-db.once('open', function(){
-	console.log('Mongoose connection successful.'); 
-}); 
+db.once('open', function () {
+	console.log('Mongoose connection successful.');
+});
 
-// Model for the URLS that get put into the form
 var Video = require('./models/Video.js'); 
+var Comment = require('./models/Comment.js');
 
-// var exampleVideo = new Video({
-// 	title: "Test Video", 
-// 	url: "https://vimeo.com/76979871", 
-// 	duration: 61.86
-// })
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// exampleVideo.save(function(err, doc){
-// 	if (err){
-// 		console.log(err);
-// 	} else {
-// 		console.log(doc);
-// 	}
-// }); 
+app.get('/', function(req, res){
+	res.sendFile('./public/index.html')
+})
 
-app.get('/', function(req, res) {
-  res.send(index.html);
+app.get('/write', function(req, res){
+
+	var newVid = new Video({
+		url: 'https://www.youtube.com/watch?v=BcsfftwLUf0', 
+		duration: '3:04', 
+		comments: 'test comment'
+	});
+
+	db.videos.save(function(err, doc) {
+	  // log any errors
+	  if (err) {
+	    console.log(err);
+	  } 
+	  // or log the doc
+	  else {
+	    console.log(doc);
+	  }
+	});
+
 });
 
-// Route to see what user looks like without populating
-app.get('/video', function(req, res) {
-  Video.find({}, function(err, doc) {
-    // send any errors to the browser
-    if (err) {
-      res.send(err);
-    } 
-    // or send the doc to the browser
-    else {
-      res.send(doc);
-    }
-  });
-});
 
-// Listen on Port 3000
-app.listen(3030, function() {
-  console.log('App running on port 3030!');
+// app.post('/submit', function(req, res){
+
+// 	var newVideo = new Video(req.body); 
+
+// 	db.videos.save(function(err, saved){
+// 		if (err) {
+// 			res.send(err);
+// 		} else {
+// 			res.send(saved);
+// 		}
+// 	});
+// });
+
+// app.get('/videos', function(req, res) {
+//   // find all videos in the Video collection
+//   Video.find({}, function(err, doc) {
+//     // send any errors to the browser
+//     if (err) {
+//       res.send(err);
+//     } 
+//     // or send the doc to the browser
+//     else {
+//       res.send(doc);
+//     }
+//   });
+// });
+
+
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
 });
